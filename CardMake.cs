53f8e793fc6b62edcfe4910ca4aa7702c8035f9a
LeeXJ <872846303@QQ.COM>
2017-12-08 16:31:04 +0800

    add

--- CardMake.cs
+++ CardMake.cs
@@ -1,24 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CardMake : MonoBehaviour
 {
     public Text PointLeft;
+    public Text PointPhy;
 
     private int point = 100;
+    private int pointphy = 50;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    PointLeft.text = string.Format("属性点剩余:{0}", point);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        PointLeft.text = string.Format("属性点剩余: {0}", point);
+        PointPhy.text = string.Format("物理攻击力: {0}", pointphy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPhysicalAdd()
+    {
+        point--;
+        pointphy++;
+        Refresh();
+    }
+
+    public void OnPhysicalRemove()
+    {
+        point++;
+        pointphy--;
+        Refresh();
+    }
 
 
 }
